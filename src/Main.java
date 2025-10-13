b86d4f6eb00ad0566fbc2c49ce30b0712e4d3069
lkimb <lkimbl@mail.ru>
2025-10-13 15:17:41 +0300

    HW-5. Done

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,52 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-
+        System.out.println("Задача 1");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Задача 2");
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Задача 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Задача 5");
+        for (int i = 1904; i <= 2096; i = i + 4) {
+            System.out.println(i + " год является високосным.");
+        }
+        System.out.println("Задача 6");
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Задача 7");
+        for (int i = 1; i <= 512; i = i * 2) {
+            System.out.println("i = " + i);
+        }
+        System.out.println("Задача 8");
+        int savings = 29000;
+        int total = 0;
+        for (int i = 0; i < 12; i++) {
+            total = total + savings;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + total + " рублей.");
+        }
+        System.out.println("Задача 9");
+        int savingsPerMonth = 29000;
+        int totalPerMonth = 0;
+        for (int i = 0; i < 12; i++) {
+            totalPerMonth = totalPerMonth + totalPerMonth / 100;
+            totalPerMonth = totalPerMonth + savingsPerMonth;
+            System.out.println("Месяц " + i + " сумма накоплений равна " + totalPerMonth + " рублей.");
+        }
+        System.out.println("Задача 10");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2 * " + i + " = " + i * 2);
+        }
     }
 }
